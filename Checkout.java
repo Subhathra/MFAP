2a8ee799622281c0376f99f0561264687cb6bfc8
Subhathra <33424557+Subhathra@users.noreply.github.com>
2017-11-06 22:15:13 +0530

    Update Checkout.java

--- Checkout.java
+++ Checkout.java
@@ -1,5 +1,5 @@
 package com.neosis;
-
+//Creating a new class for checkout, In this login to the filpkart site and search for desired products add to the bag and logoff
 public class Checkout {
 
 }
