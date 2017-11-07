03920c40bf40c7495db25938369c49f7e4bb7621
subhathra <subhathra.subhathra.r@gmail.com>
2017-11-07 16:05:01 +0530

    adding these files for test

--- Checkout.java
+++ Checkout.java
@@ -1,5 +1,5 @@
 package com.neosis;
-//Creating a new class for checkout, In this login to the filpkart site and search for desired products add to the bag and logoff
+//Creating a new class for checkout, In this login to the amazon site and search for desired products add to the bag and logoff
 public class Checkout {
 
 }
